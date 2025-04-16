42431a66caa8cdb26c689bdb0d9b94b54c15bdba
RicardoHC28 <ricardohuamanhc28@gmail.com>
2025-04-15 22:49:02 -0500

    Creacion de seccion de recomendaciones y Footer

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import RecommendedTours from '../layout/RecommendedTours';
 
 export default function HomePage() {
     return (
         <div>
             <h1>Home Page</h1>
+            <RecommendedTours />
         </div>
     )
 }
--- src/pages/LayoutPage.js
+++ src/pages/LayoutPage.js
@@ -1,5 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "../layout/Header";
+import Footer from "../layout/Footer";
+
 
 export default function LayoutPage () {
 
@@ -13,7 +15,7 @@ export default function LayoutPage () {
                 <Outlet/>
             </main>
 
-            <h1>Footer</h1>
+            <Footer/>
 
         </>
 
