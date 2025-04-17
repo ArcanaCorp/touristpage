12b941773e5c6cf8b267a541b276f8c732d60d37
Anderson <andersonhuamanimachuca@gmail.com>
2025-04-16 20:55:43 -0500

    Resolviendo conflicto de merge en HomePage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,21 +1,14 @@
-import React from 'react'
-<<<<<<< HEAD
-import Gallery from '../layout/Gallery'
-=======
+import React from 'react';
+import Gallery from '../layout/Gallery';
 import RecommendedTours from '../layout/RecommendedTours';
->>>>>>> 42431a66caa8cdb26c689bdb0d9b94b54c15bdba
 
 export default function HomePage() {
     return (
         <div>
             <h1>Home Page</h1>
-<<<<<<< HEAD
             <Gallery />
-            <h2>Footer</h2>
-=======
             <RecommendedTours />
->>>>>>> 42431a66caa8cdb26c689bdb0d9b94b54c15bdba
+            <h2>Footer</h2>
         </div>
-    )
+    );
 }
-
